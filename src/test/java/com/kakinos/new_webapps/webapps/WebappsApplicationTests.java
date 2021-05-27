7dc5798a344215fc8d47e33ecee399f4ab85497b
arun <rawatarun1592@gmail.com>
2021-05-27 20:02:59 +0530

    new Project

---
+++ src/test/java/com/kakinos/new_webapps/webapps/WebappsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakinos.new_webapps.webapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
